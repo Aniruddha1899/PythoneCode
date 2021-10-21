8185080392103e66a55b69a78277311ca55374c9
Aniruddha Tiwari <aniruddha.tiwari1807@gmail.com>
2021-10-21 13:51:23 -0400

    Compare the first and last element in the string

--- Palindrome.py
+++ Palindrome.py
@@ -5,3 +5,23 @@ def isPalindrome(n):
     # we are finding the reverse of the string 
     # and checkong if the reverse and the original are same or not.
 print(isPalindrome("racecar"))
+
+# new method
+
+def isPalindrome1(n):
+    j=-1
+    flag=0
+    # compares the first and last element of the string and give the rest of the substring
+    # to a recursive call to itself
+    for i in n:
+        if i!= n[j]:
+            j = j -1
+            flag = 1
+            break
+        j = j -1
+    if flag ==1:
+        print(" Not a Palindrome")
+    else:
+        print("Is a Palindrome")
+
+isPalindrome1("Radar")
