2465ab16729ab974b85658183aff114b6aa3210a
Aniruddha Tiwari <aniruddha.tiwari1807@gmail.com>
2021-10-23 15:17:37 -0400

    updatedVersion

--- S3files.py
+++ S3files.py
@@ -5,16 +5,16 @@ from io import StringIO
 # Creating the low level functional client
 client = boto3.client(
     's3',
-    aws_access_key_id = 'AKIA3PU3C66B2LNYPL7U',
-    aws_secret_access_key = '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',
+    aws_access_key_id = '**************',
+    aws_secret_access_key = '**************',
     region_name = 'us-east-2'
 )
-    
+
 # Creating the high level object oriented interface
 resource = boto3.resource(
     's3',
-    aws_access_key_id = 'AKIA3PU3C66B2LNYPL7U',
-    aws_secret_access_key = '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',
+    aws_access_key_id = '**************',
+    aws_secret_access_key = '**************',
     region_name = 'us-east-2'
 )
 
